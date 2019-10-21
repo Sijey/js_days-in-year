9335b39be4c7a63c5064445d1c9b8af35c6c8e4a
Sijey <Popov040391@gmail.com>
2019-10-21 13:08:12 +0300

    Solution

---
+++ script.js
@@ -0,0 +1,11 @@
+function daysInYear(year) {
+    try {
+        if (!Number.isInteger(year)) {
+            throw new Error();
+        }
+
+        return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0) ? 366 : 365;
+    } catch (err) {
+        return 'exception';
+    }
+}
